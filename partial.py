8529d898f97dd9539ca3225a2af56fd1f53113e4
2022BCS0020-JAYANTH <kurivella22bcs20@iiitkottayam.ac.in>
2026-03-15 12:33:39 +0530

    Delete partial.py

--- partial.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv("data/housing.csv")
-df_small = df.head(5000)
-df_small.to_csv("data/housing.csv", index=False)
-print("Partial dataset created with", len(df_small), "rows")
